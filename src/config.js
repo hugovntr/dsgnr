06fe5b044fbdc689f0c452a2af3e03e16466ed2f
BLVCKLNGS <hugo.poullet@epitech.eu>
2019-01-22 20:22:05 +0100

    Vue App config update

--- src/config.js
+++ src/config.js
@@ -1,29 +1,28 @@
 function getConfig() {
-	this.API_LOCATION = (process.env.NODE_ENV === "staging") 
-		? 'http://api.dsgnr.lab/v1/'
-		: 'http://127.0.0.1/dsgnr/api/v1',
+	let API_LOCATION = process.env.VUE_APP_API_URL || 'http://127.0.0.1/dsngr/api/v1';
+	
 	this.api = {
 		_getImages: (page = 1) => {
 			return {
 				method: 'GET',
-				url: this.API_LOCATION + 'images',
-				pages: this.API_LOCATION + 'images?page=' + page,
+				url: API_LOCATION + 'images',
+				pages: API_LOCATION + 'images?page=' + page,
 			};
 		},
 		_getImage: (slug) => {
 			return {
 				method: 'GET',
-				url: this.API_LOCATION + 'images/' + slug
+				url: API_LOCATION + 'images/' + slug
 			};
 		},
 		_getSlugs: {
 			method: 'GET', 
-			url: this.API_LOCATION + 'images/?type=slugs',
+			url: API_LOCATION + 'images/?type=slugs',
 		},
 		_editImage: (slug) => {
 			return {
 				method: 'PUT',
-				url: this.API_LOCATION + 'images/' + slug
+				url: API_LOCATION + 'images/' + slug
 			};
 		}
 	}
